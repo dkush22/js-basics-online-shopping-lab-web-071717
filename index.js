1d3e4b9ba8d1f0f9e7d76ea4765b69eab234ca13
dkush22 <dkush22@gmail.com>
2017-08-28 17:09:25 -0400

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,67 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemObject = {}
+ itemObject[item] = Math.floor(Math.random() * 100)
+ cart.push(itemObject)
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 1) {
+for (let i = 0; i < cart.length; i++) {
+	let key = Object.keys(cart[i]);
+	let val = cart[i][key];
+	console.log(`In your cart, you have ${key} at $${val}.` )
+}} else if (cart.length === 2) {
+	let key = Object.keys(cart)
+	console.log(`In your cart, you have ${Object.keys(cart[0])[0]} at $${cart[0][Object.keys(cart[0])[0]]} and ${Object.keys(cart[1])[0]} at $${cart[1][Object.keys(cart[1])[0]]}.`)
+} else if (cart.length >= 3){
+let key = Object.keys(cart)
+let str = "In your cart, you have"
+for (let i = 0; i < cart.length; i++) {
+	if (i === cart.length - 1){
+		let thiskey = Object.keys(cart[i])[0]
+		str += ` and ${thiskey} at $${cart[i][thiskey]}.`
+	} else {
+		let thiskey = Object.keys(cart[i])[0]
+		str += ` ${thiskey} at $${cart[i][thiskey]},`
+	}
+	console.log(str)
+}
+}else{
+  console.log("Your shopping cart is empty.")
+}
 }
 
 function total() {
-  // write your code here
+	let sum = 0
+ for (let i = 0; i < cart.length; i++) {
+ 	let key = Object.keys(cart[i])[0]
+ sum += cart[i][key]
+}
+return sum
 }
 
 function removeFromCart(item) {
   // write your code here
+   for (let i = 0; i < cart.length; i++) {
+        if (cart[i].hasOwnProperty(item)) {
+          cart.splice(i,1);
+          return cart;
+        }
+    }
+    console.log('That item is not in your cart.');
+    return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+	if (!cardNumber) {
+		console.log("Sorry, we don't have a credit card on file for you.")
+	} else {
+		console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+		cart.length = 0
+	}
+
 }
